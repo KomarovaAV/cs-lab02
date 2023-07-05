65acd3eed5623960035ab62ae2107ee27f5bef51
Ann (KomarovaAV) <dusk1416@mail.ru>
2023-07-05 14:19:02 +0300

    code: добавили среднее

--- main.cpp
+++ main.cpp
@@ -10,7 +10,8 @@ cin >> a >> b;
 cout << "A + B = " << a + b << '\n'
      << "A - B = " << a - b << '\n'
      << "A * B = " << a * b << '\n'
-     << "A / B = " << a / b << '\n';
+     << "A / B = " << a / b << '\n'
+     << "A % B = " << (a + b) / 2 << '\n';
 int max = (a > b) ? a : b;
 cout << "max: " << max << endl;
 
